10b0c88d6026dcbd973ebd2428bc6d9b6444e2fb
dasaridevisrivaraprasad <dasaridevisrivaraprasad@gmail.com>
2025-03-26 17:10:33 +0530

    updated successfully

---
+++ MyPortfolio/script1.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (e) {
+        e.preventDefault();
+        alert("Message Sent!");
+    });
+});
