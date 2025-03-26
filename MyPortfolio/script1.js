05552c6915648186d2e7cb23d9a7a92cfb78bd26
Dasari Devi Sri Vara Prasad <128198004+dasaridevisrivaraprasad@users.noreply.github.com>
2025-03-26 17:13:26 +0530

    Delete MyPortfolio directory

--- MyPortfolio/script1.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("form").addEventListener("submit", function (e) {
-        e.preventDefault();
-        alert("Message Sent!");
-    });
-});
